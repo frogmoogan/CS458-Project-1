03fdb6135ac7b1427cdf7c84066727e4926695e8
frogmoogan <meng5@binghamton.edu>
2026-02-18 01:24:06 -0500

    added quarterround, figuring out i/o stream

--- Main.java
+++ Main.java
@@ -1,5 +1,27 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello World");
-  }
-}
+    // Checking if length of args array is
+        // greater than 0
+        if (args.length > 0) {
+
+            System.out.println("Enter your key length 
+            the key represented as a hezadecimal string, nonce(IV)
+            represented as a hexadecimal string, and the text
+            string to be encrypted/decrypted in hexadecimal format:");
+            // Print statements
+            System.out.println("The command line"
+                               + " arguments are:");
+
+            // Iterating the args array
+            // using for each loop
+            for (String val : args)
+
+                System.out.println(val);
+        }
+        else{
+
+            System.out.println("No command line "
+                               + "arguments found.");
+        }
+    }
+ }
---
+++ quarterround.java
@@ -0,0 +1,7 @@
+public class quarterround {
+    
+    public quarterround(){
+
+        
+    }
+}
