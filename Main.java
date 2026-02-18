d8b4f3bd7b081d3b164a258c39d97c3ea4c1e6ab
frogmoogan <meng5@binghamton.edu>
2026-02-18 01:55:16 -0500

    put all the functions in main (?) figure out quarterround best way to bit shift

--- Main.java
+++ Main.java
@@ -1,23 +1,54 @@
 public class Main {
+    int keysize = 0;
+    String stringkey = "";
+    String nonce = "";
+    String plaintxt = "";
+    //not needed necessarily
+    //String ciphertxt = "";
+
+    /* 
+    static int quarterround(y0,y1,y2,y3) {
+        //whatever quarterround does
+        //come back to this later
+         
+  }
+  */
+
   public static void main(String[] args) {
-    // Checking if length of args array is
+        Main myObj = new Main();
+        // Checking if length of args array is
         // greater than 0
         if (args.length > 0) {
 
-            System.out.println("Enter your key length as a hexadecimal string, nonce(IV) represented as a hexadecimal string, and the textstring to be encrypted/decrypted in hexadecimal format:");
             // Print statements
-            System.out.println("The command line"
-                               + " arguments are:");
+            /* 
+            System.out.println("The command line arguments are:");
 
             // using for each loop
             for (String val : args)
-
                 System.out.println(val);
+            */
         }
         else{
 
-            System.out.println("No command line "
-                               + "arguments found.");
+            System.out.println("No command line arguments found.");
         }
+
+        //reassigning values
+        myObj.keysize = Integer.valueOf(args[0]);
+        myObj.stringkey = args[1];
+        myObj.nonce = args[2];
+        myObj.plaintxt = args[3];
+        //myObj.ciphertxt = args[4];
+
+        //print check reassigning workability
+        /*
+        System.out.println("keysize : " + myObj.keysize);
+        System.out.println("stringkey : " + myObj.stringkey);
+        System.out.println("nonce : " + myObj.nonce);
+        System.out.println("plaintxt : " + myObj.plaintxt);
+        */
+        //System.out.println("ciphertxt : " + myObj.ciphertxt);
+
     }
  }
--- quarterround.java
+++
@@ -1,7 +0,0 @@
-public class quarterround {
-    
-    public quarterround(){
-
-        
-    }
-}
